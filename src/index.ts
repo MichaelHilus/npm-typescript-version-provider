aaba606c8b96dc626b68ced59eaf2ae4c81d483c
Michael Hilus <m.hilus@progresso-group.de>
2019-09-19 10:37:09 +0200

    Build in version loader and version injector

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-import { createAbsolutePath } from './pathCreator';
+import * as fs from 'fs';
 
-class Provider {
-    public get message(): string {
-        return 'This is typescript.';
+import { PathCreator } from './pathCreator';
+import { ArgsHandler } from './argsHandler';
+import { VersionLoader } from './versionLoader';
+import { VersionInjector } from './versionInjector';
+
+function provide() {
+    const argsHandler = new ArgsHandler(process.argv);
+    const validationResult = argsHandler.validate();
+    if (!validationResult.isValid) {
+        console.log(validationResult.errorMessage);
+        return;
     }
+
+    const classPath = new PathCreator().getAbsolutePath(argsHandler.args[0]);
+
+    if (!fs.existsSync(classPath)) {
+        console.log(`Error: file ${classPath} was not found. Please create a typescript class first.`);
+        return;
+    }
+
+    const version = new VersionLoader().version;
+
+    const versionInjector = new VersionInjector(version, classPath);
+    versionInjector.inject();
 }
 
-console.log(new Provider().message);
-console.log(`path: ${createAbsolutePath('/src/services/info-service.ts')}`)
+provide();
